b5124054c494df6d25c29adff28cb3915344e566
jpliu <jpliu@freewheel.com>
2020-05-18 21:49:10 +0800

    false sharing of java tech

---
+++ src/main/java/org/lkpnotice/infra/falseshareing/FalseSharing.java
@@ -0,0 +1,59 @@
+package org.lkpnotice.infra.falseshareing;
+
+
+import sun.misc.Contended;
+
+/**
+ * Created by jpliu on 2020/5/18.
+ */
+public class FalseSharing  implements Runnable {
+    public static int NUM_THREADS = 4;
+    public final static long ITERATIONS = 500L * 1000L * 1000L;
+    private final int arrayIndex;
+    private static VolatileLong[] longs;
+
+    public FalseSharing(final int arrayIndex) {
+        this.arrayIndex = arrayIndex;
+    }
+
+    public static void main(final String[] args) throws Exception {
+        System.out.println("starting....");
+        longs = new VolatileLong[NUM_THREADS];
+        for (int i = 0; i < longs.length; i++) {
+            longs[i] = new VolatileLong();
+        }
+        final long start = System.nanoTime();
+        runTest();
+        System.out.println("duration = " + (System.nanoTime() - start));
+    }
+
+    private static void runTest() throws InterruptedException {
+        Thread[] threads = new Thread[NUM_THREADS];
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(new FalseSharing(i));
+        }
+        for (Thread t : threads) {
+            t.start();
+        }
+        for (Thread t : threads) {
+            t.join();
+        }
+    }
+
+    public void run() {
+        long i = ITERATIONS;
+        while (0 != --i) {
+            longs[arrayIndex].value = i;
+        }
+    }
+
+    //@Contended //注释用来测试
+    public final static class VolatileLong {
+        public volatile long value = 0L;
+//        public long p1, p2, p3, p4, p5, p6;
+
+//        public long getValue() {
+//            return p1 + p2 + p3+ p4 +p5+p6;
+//        }
+    }
+}
